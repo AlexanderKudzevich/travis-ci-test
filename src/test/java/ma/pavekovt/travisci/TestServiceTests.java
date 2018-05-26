10e4973f734c361287e8994490c36ba3ee7a274f
pavekovt <39086668+pavekovt@users.noreply.github.com>
2018-05-26 11:12:01 +0100

    Travis integration (#3)
    Travis changes

--- src/test/java/ma/pavekovt/travisci/TestServiceTests.java
+++ src/test/java/ma/pavekovt/travisci/TestServiceTests.java
@@ -3,17 +3,20 @@ package ma.pavekovt.travisci;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import me.pavekovt.travisci.TestService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
+@RunWith(BlockJUnit4ClassRunner.class)
 public class TestServiceTests {
 
   @Test
-  void TestExpectedWorkOutput() {
+  public void TestExpectedWorkOutput() {
     final String expected = "expected";
     TestService testService = new TestService(expected);
 
     assertThat(testService.doWork())
-        .isSameAs(expected);
+        .isEqualTo("work output: " + expected);
   }
 
 }
